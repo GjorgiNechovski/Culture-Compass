8ce44f1fd6a5df0885f00b52a542ba2cfea1674f
GjorgiNechovski <gjorginechovski@gmail.com>
2024-01-25 12:18:31 +0100

    Brought back the old API link

--- Domashna 4/Frontend/src/app/const-variables.models.ts
+++ Domashna 4/Frontend/src/app/const-variables.models.ts
@@ -1,3 +1,1 @@
-// export const appApi = 'https://culture-compass.azurewebsites.net/api';
-
-export const appApi = 'http://localhost:8080/api';
+export const appApi = 'https://culture-compass.azurewebsites.net/api';
